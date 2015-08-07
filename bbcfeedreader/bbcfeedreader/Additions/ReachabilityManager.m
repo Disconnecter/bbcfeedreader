10948cdf52b97d1d754e7efe7e59a0277dea1afa
Sergey Zabolotnyy <zabolotnyy.s@gmail.com>
2015-08-07 14:30:28 +0300

    reachability

---
+++ bbcfeedreader/bbcfeedreader/Additions/ReachabilityManager.m
@@ -0,0 +1,13 @@
+//
+//  ReachabilityManager.m
+//  bbcfeedreader
+//
+//  Created by Zabolotnyy S. on 07.08.15.
+//  Copyright (c) 2015 Zabolotnyy S. All rights reserved.
+//
+
+#import "ReachabilityManager.h"
+
+@implementation ReachabilityManager
+
+@end
